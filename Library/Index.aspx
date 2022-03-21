7ecd23000747cc130ce65bd3f7c8ce67b6d84ab0
PiersonTobeck <50812223+PiersonTobeck@users.noreply.github.com>
2022-03-21 14:54:52 +1300

    Crease css and html files

---
+++ Library/Index.aspx
@@ -0,0 +1,88 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="Index.aspx.vb" Inherits="Library.Index" %>
+
+<!doctype html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head runat="server">
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <meta name="keywords" content="" />	<!-- add keywords -->
+    <meta name="description" content="" />	<!-- add description content -->
+    <meta name="author" content="" />		<!-- Add your name to 'author' -->
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <title>St. Georges Restaurant</title>
+
+    <!-- Links for css, bootstrap -->
+    <link href="css/library.css" rel="stylesheet" type="text/css" />	<!-- edit the file name -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />	<!-- edit the file name -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <!-- Navigation starts below -->
+    <div class="navbar navbar-default" role="navigation">
+	    <div class="navbar-header">
+	
+		    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+			    <span class="icon-bar"></span>
+			    <span class="icon-bar"></span>
+			    <span class="icon-bar"></span>
+		    </button>
+				
+		    <!-- Logo goes here -->
+			
+		    <h1> Library </h1>
+	    </div> <!-- End of navbar-header -->
+	
+	    <div class="container">  <!-- Top links start here -->
+		    <div class="navbar-collapse collapse">
+			    <ul class="nav navbar-nav navbar-right">
+				    <li class="active"><a href="index.aspx">Bookings</a></li>
+				    <li><a href=""></a>Search</li>
+				    <li><a href=""></a>Contact</li>	
+			    </ul>
+		    </div> <!-- End of navbar collapse -->
+	    </div> <!-- End of navigation container class -->
+    </div>
+    <!-- Navigation ends above -->
+
+<!-- Main content div starts below -->
+    <div class="container">
+	    <div class="main">
+
+	        <h1>Booking Form</h1>
+
+            <form id ="form1" runat="server"
+
+                <asp:Panel ID="pnlLibrary"
+
+             <!-- End booking form -->
+	
+	    </div> <!-- Main content end  -->
+	   
+    </div> <!-- container (around main div) end-->
+
+     <!-- Footer starts below -->
+	 <footer class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+        <div class="container">
+            <div class="navbar-text pull-right">
+                &copy; Pierson  &amp; 2022  
+            </div>
+        </div>
+    </footer>
+    <!-- Footer ends above -->
+ 
+
+<!-- Link to JavaScipt plugins, needed for things like the dropdown menu to work.  BOTH files are required. -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    
+    <script src="js/bootstrap.min.js"></script>
+   
+</body>
+</html>
---
+++ Library/book.vb
@@ -0,0 +1,162 @@
+﻿Public Class Book
+
+    'Declaration of variables
+
+    Private Title As String
+    Private Author As String
+    Private Publisher As String
+    Private Isbn As Double
+    Private EstValue As Double
+
+    'Constructors
+
+    Public Sub New(a As String, b As String, c As String, d As Double, e As Double)
+
+        Me.Title = a
+        Me.Author = b
+        Me.Publisher = c
+        Me.Isbn = d
+        Me.EstValue = e
+
+    End Sub
+
+    Public Sub New()
+
+    End Sub
+
+    'Methods
+
+    'Getters
+
+    ''' <summary>
+    ''' Returns the title of selected book
+    ''' </summary>
+    ''' <returns> string </returns>
+    Public Function GetTitle() As String
+
+        Return Me.Title
+
+    End Function
+
+    ''' <summary>
+    ''' Returns the Author of selected book
+    ''' </summary>
+    ''' <returns> string </returns>
+
+    Public Function GetAuthor() As String
+
+        Return Me.Author
+
+    End Function
+
+    ''' <summary>
+    ''' Returns the Publisher of selected book
+    ''' </summary>
+    ''' <returns> string </returns>
+
+    Public Function GetPublisher() As String
+
+        Return Me.Publisher
+
+    End Function
+
+    ''' <summary>
+    ''' Returns the ISBN of selected book
+    ''' </summary>
+    ''' <returns> Double </returns>
+
+    Public Function GetIsbn() As Double
+
+        Return Me.Isbn
+
+    End Function
+
+    ''' <summary>
+    ''' Returns the Estimated Value of selected book
+    ''' </summary>
+    ''' <returns> Double </returns>
+
+    Public Function GetEstValue() As Double
+
+        Return Me.EstValue
+
+    End Function
+
+    ''' <summary>
+    ''' Returns all information of selected book 
+    ''' </summary>
+    ''' <returns> string </returns>
+    Public Function GetBookInfo()
+
+        Return String.Format("Title: " & Me.GetTitle &
+                             Environment.NewLine + " Author: " & Me.GetAuthor &
+                             Environment.NewLine + " Publisher: " & Me.GetPublisher &
+                             Environment.NewLine + " Isbn: " & Me.GetIsbn &
+                             Environment.NewLine + " EstValue: " & Me.GetEstValue)
+
+    End Function
+
+    'Setters
+    ''' <summary>
+    ''' Sets the title of selected book using user input
+    ''' </summary>
+    ''' <returns> Nothing </returns>
+    Public Function SetTitle(x As String)
+
+        Me.Title = x
+
+        Return Nothing
+
+    End Function
+
+    ''' <summary>
+    ''' Sets the author of selected book using user input
+    ''' </summary>
+    ''' <returns> Nothing </returns>
+
+    Public Function SetAuthor(x As String)
+
+        Me.Author = x
+
+        Return Nothing
+
+    End Function
+
+    ''' <summary>
+    ''' Sets the publisher of selected book using user input
+    ''' </summary>
+    ''' <returns> Nothing </returns>
+    ''' 
+    Public Function SetPublisher(x As String)
+
+        Me.Publisher = x
+
+        Return Nothing
+
+    End Function
+
+    ''' <summary>
+    ''' Sets the ISBN of selected book using user input
+    ''' </summary>
+    ''' <returns> Nothing </returns>
+    Public Function SetIsbn(x As Double)
+
+        Me.Isbn = x
+
+        Return Nothing
+
+    End Function
+
+    ''' <summary>
+    ''' Sets the estimated value of selected book using user input
+    ''' </summary>
+    ''' <returns> Nothing </returns>
+    Public Function SetEstValue(x As Double)
+
+        Me.EstValue = x
+
+        Return Nothing
+
+    End Function
+
+End Class
